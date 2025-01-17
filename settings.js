fe6249a4b11cbb7a039f637b3509d2347201fcad
Trigger957 <amponsahyawtabi82@gmail.com>
2025-01-17 18:39:00 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "233536979044"
+global.pairingNumber = "233535611900"
 
 
 
